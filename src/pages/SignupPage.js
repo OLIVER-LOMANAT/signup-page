1f7ddf713f837fd362a0c794ee0220fbe083558f
Oliver Lomanat <lomanatoliver@gmail.com>
2025-10-06 14:14:58 +0300

    Adding login page

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,11 +1,19 @@
 import { User, Lock, Mail, MessageSquare } from 'lucide-react';
-import React from 'react'
+import React, { useState } from 'react'
 
 function SignupPage() {
+    const [isLogin, setIsLogin] = useState(false)
+
+    function toggleAuthUser() {
+        setIsLogin(!isLogin)
+    }
   return (
-    <div className='flex min-h-screen'>
-        <div className='flex-1 flex justify-center items-center'>
-            <div className='w-full max-w-md'>
+    <div className='flex min-h-screen overflow-hidden'>
+        <div className={`flex-1 flex justify-center items-center transition-all duration-700 ease-out
+            ${isLogin ? 'translate-x-full' : 'translate-x-0'}`}>
+            <div className='w-full max-w-md p-6'>
+                {isLogin ? (
+                <>
                 <div className='text-center'>
                     <h1 className='text-2xl font-bold mb-2'>Create Account</h1>
                     <p className='text-gray-500 mb-6'>Hey There I Forgot You! Haha</p>
@@ -34,14 +42,44 @@ function SignupPage() {
                     </div>
                     <div className='text-center'>
                         <button className='w-full text-white font-bold px-3 py-2 mt-3 mb-3 bg-purple-500 rounded-md hover:bg-purple-300 hover:text-black transition duration-200'>CREATE ACCOUNT</button>
-                        <p className=''>Already have an account? <a className='text-purple-700 hover:underline'>Sign in</a></p>
+                        <p>Already have an account? <button className='text-purple-700 hover:underline' onClick={toggleAuthUser}>Sign in</button></p>
                     </div>
                 </form>
+                </>
+                ) : (
+                <>
+                <div className='text-center'>
+                    <h1 className='text-2xl font-bold mb-2'>Welcome Back</h1>
+                    <p className='text-gray-500 mb-6'>Hey There I got forgoten too! Damn</p>
+                </div>
+                <form>
+                    <div className='mb-2'>
+                        <label className='block text-gray-700 mt-2'>Fullname:</label>
+                        <div className='relative'>
+                            <User className="absolute left-3 top-1/2 text-gray-400 transform -translate-y-1/2" size={20}/>
+                            <input type='text' className='w-full pl-12 px-3 py-2 border border-gray-400 rounded-md' placeholder='didaloma' required/>
+                        </div>
+                    </div>
+                    <div className='mb-2'>
+                        <label className='block text-gray-700 mt-2'>Password:</label>
+                        <div className='relative'>
+                            <User className="absolute left-3 top-1/2 text-gray-400 transform -translate-y-1/2" size={20}/>
+                            <input type='password' className='w-full pl-12 px-3 py-2 border border-gray-400 rounded-md' placeholder='******' required/>
+                        </div>
+                    </div>
+                    <div>
+                        <button className='w-full text-white font-bold px-3 py-2 mt-3 mb-3 bg-purple-500 rounded-md hover:bg-purple-300 hover:text-black transition duration-200'>Login</button>
+                        <p className='text-center'>Create an account? <button className='text-purple-700 hover:underline' onClick={toggleAuthUser}>create account</button></p>
+                    </div>
+                </form>
+                </>
+                )}
             </div>
         </div>
-        <div className='flex-1 bg-purple-500 text-white flex justify-center items-center'>
-            <div className='text-center max-w-md'>
-                    <MessageSquare className='mx-auto mb-4' size={54}/>
+        <div className={`flex-1 bg-purple-500 text-white flex justify-center items-center transition-all duration-700 ease-in-out
+            ${isLogin ? '-translate-x-full': 'translate-x-0'}`}>
+            <div className='text-center  max-w-md p-6'>
+                    <MessageSquare className='mx-auto mb-4 animate-bounce' size={54}/>
                     <h1 className='text-3xl font-bold mb4'>Join our community</h1>
                     <p className='text-xl'>Connect with friends, share moments, and stay in touch with your loved ones.</p>
             </div>
