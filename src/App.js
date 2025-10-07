0a69f8d20721c99f2543c80555a41ebb10ae4a33
Oliver Lomanat <lomanatoliver@gmail.com>
2025-10-07 17:15:13 +0300

    Adding files for

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div>
       <SignupPage />
+
+       <ToastContainer 
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
---
+++ src/lib/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000'
+
+export const axiosInstance = axios.create({
+    baseURL: `${API_BASE_URL}/api`,
+    withCredentials: true,
+});
